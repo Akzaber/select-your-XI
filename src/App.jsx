1e89016155b3483e4023780c8fa9f271a5128c11
AK Zaber <zaberkhan611@gmail.com>
2026-03-12 07:06:35 +0600

    complete the project

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const playersFetch = async () => {
 const playersPromise = playersFetch();
 function App() {
   const [toggle, setToggle] = useState(true);
-  const [availableBalance, setAvailableBalance] = useState(10000000);
+  const [availableBalance, setAvailableBalance] = useState(100000000);
   const [purchasedPlayers, setPurchasedPlayers] = useState([]);
   console.log(purchasedPlayers);
   const removePlayer = (id, amount) => {
@@ -25,7 +25,7 @@ function App() {
       <div className='max-w-[1200px] mx-auto'>
         <Navbar availableBalance={availableBalance}></Navbar>
         <div className='flex justify-between items-center mt-3 border-b-2 pb-4 border-b-gray-200'>
-                <h1 className='text-3xl font-bold'> {`${toggle? `Available Players` : `Selected Players ${purchasedPlayers.length}/8`}`}</h1>
+                <h1 className='text-3xl font-bold'> {`${toggle? `Available Players` : `Selected Players ${purchasedPlayers.length}/6`}`}</h1>
                 <div>
                     <button onClick={() => setToggle(true)} className={`px-4 py-2 hover:cursor-pointer rounded-l-xl border border-r-0 font-semibold ${toggle === true? 'bg-[#E7FE29]' : ''}`}>Available</button>
                     <button onClick={() => setToggle(false)} className={`px-4 py-2 hover:cursor-pointer border border-l-0 rounded-r-xl font-semibold ${toggle === false? 'bg-[#E7FE29]' : ''}`}>Selected <span>({purchasedPlayers.length})</span></button>
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,12 +1,16 @@
 import SelectedPlayer from '../SelectedPlayer/SelectedPlayer';
 
-const SelectedPlayers = ({purchasedPlayers, removePlayer}) => {
+const SelectedPlayers = ({ purchasedPlayers, removePlayer }) => {
     return (
-        <div className='space-y-4 mt-4'>
-            {
-                purchasedPlayers.map(player => <SelectedPlayer removePlayer={removePlayer} key={player.id} player={player}></SelectedPlayer>)
-            }
+        <div>
+            <div className='space-y-4 mt-4'>
+                {
+                    purchasedPlayers.map(player => <SelectedPlayer removePlayer={removePlayer} key={player.id} player={player}></SelectedPlayer>)
+                }
+            </div>
+            <button className='px-4 mt-6 py-2 bg-[#E7FE29] rounded font-semibold'>Add More Player</button>
         </div>
+
     );
 };
 
